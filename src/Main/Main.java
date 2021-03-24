1fe76b2f5d24c1f01da438dafbc2e1fb58a3a6de
IvanGer212 <Comradeivan88@gmail.com>
2021-03-24 17:46:21 +0300

    Add countTimeForProgramInTwoStream

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,7 +5,7 @@ import java.util.SortedMap;
 public class Main {
 
     public static void main(String[] args) {
-        final int size = 1000000;
+        final int size = 10;
         final int half = size / 2;
         float[] array = new float[size];
         long startTime = 0, endTime = 0;
@@ -20,12 +20,23 @@ public class Main {
     static void countTimeForProgramInTwoStream(float[] array, long startTime, long endTime, int size){
         float[] a1 = new float[size];
         float[] a2 = new float[size];
-        Thread thread1 = new Thread();
-
-
         startTime = System.currentTimeMillis();
         System.arraycopy(array,0,a1,0,size);
         System.arraycopy(array,size,a2,0,size);
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doOperationArray(a1);
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doOperationArray(a2);
+            }
+        });
+        thread1.start();
+        thread2.start();
 
         System.arraycopy(a1,0,array,0,size);
         System.arraycopy(a2,0,array,size,size);
@@ -50,9 +61,13 @@ public class Main {
     static float[] doOperationArray(float[] inArray){
             for (int i = 0; i <inArray.length ; i++) {
                inArray[i] = (float)(inArray[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                System.out.printf(inArray[i]+" ");
             }
+        System.out.println();
             return inArray;
         }
 
 
+
 }
+
